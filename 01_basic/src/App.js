bf906200825a8a2355ab2540795127511dc95313
Pankaj <pankaj.ky3007@gmail.com>
2023-09-03 14:18:14 +0530

    understandign the react flow and structure  01 &02

--- 01_basic/src/App.js
+++ 01_basic/src/App.js
@@ -1,8 +1,10 @@
+import Chai from "./Chai";
 
 function App() {
   return (
     <div className="App">
       <h1>Chai aur Code</h1>
+      <Chai/>
     </div>
   );
 }
--- 01_vitereact/src/App.jsx
+++ 01_vitereact/src/App.jsx
@@ -1,8 +1,15 @@
-
+//importing 
+import Chai from "./chai"
 
 function App() {
   return (
-    <h1>Chai aur code with vite</h1>
+    <div>
+      // <h1>Chai aur code with vite</h1>
+    <Chai/>
+    <h1>This should be wrapped</h1>
+    </div>
+    //note that: <div></div> may be written as <>Some other tag </> it is called fragment there will no be any error.
+    
   )
 }
 
