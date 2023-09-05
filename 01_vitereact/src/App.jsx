e54057b1f8946a676ea3f3fc2a7e33877ba6aa99
Pankaj <pankaj.ky3007@gmail.com>
2023-09-05 11:38:13 +0530

    some modifications after creating customReact

--- 01_vitereact/src/App.jsx
+++ 01_vitereact/src/App.jsx
@@ -2,9 +2,11 @@
 import Chai from "./chai"
 
 function App() {
+  const username="username: chai aur code"
   return (
     <div>
-      // <h1>Chai aur code with vite</h1>
+       <h1>Chai aur code with vite {username}</h1>
+       {/* {username} -->it must be evaluated expression: here we cannot for or if etc equation. */}
     <Chai/>
     <h1>This should be wrapped</h1>
     </div>
--- 01_vitereact/src/main.jsx
+++ 01_vitereact/src/main.jsx
@@ -1,9 +1,51 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+
 import App from './App.jsx'
+
+
+
+function MyApp(){
+    return(
+        <div>
+            <h1>Custom App | Chai aur code</h1>
+        </div>
+    )
+}
+
+// const ReactElement={
+//     type:'a',//type determin the type of tag used
+//     props:{//props determine properties return object
+//         href:'https://google.com',
+//         target:'_blank'
+//     },
+//     children:'Click me to visit google'
+// } //why it is not running?
+//Ans: there is some rule according to which react work.
+
+const anotherElement=(
+    <a href='https://google.com' target='_blank'>visit google</a>
+)
+
+const anotherUser="chai aur react"
+
+const ReactElement=React.createElement(
+    'a',//first parm: tah name
+    {href:'https://google.com',target:"_blank"},'click me to visit google',
+    anotherUser
+
+)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 
-    <App />
+    // <App />
+    
+    //<MyApp/>
+    //MyApp() :--> can be called in this manner because MyApp is a function. but we should not use it .
+    //App()
+
+    // anotherElement
+     ReactElement
+
     
 )
